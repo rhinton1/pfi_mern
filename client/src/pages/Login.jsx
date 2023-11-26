81d678adaae01633693eaa8b715f9d60a425f79a
Ron Hinton <ron_hinton@yahoo.com>
2023-11-26 10:48:13 -0500

    pfi_Pilot_1: add UI for the Register and Login pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='text-center'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Login</h1>
+      <form className="flex flex-col gap-4 p-3 max-w-lg mx-auto">
+        <input type="text" placeholder="email" className="border p-3 rounded-lg" id="email" />
+        <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password" />
+        <button className="bg-slate-600 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign In</button>
+      </form>
+      <div className='flex gap-2 items-center justify-center'>
+        <p>Dont have an account?</p>
+        <Link to={'/register'}><span className='text-blue-700'>Register</span></Link>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='text-center'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Create a New Account</h1>
+      <form className="flex flex-col gap-4 p-3 max-w-lg mx-auto">
+        <input type="text" placeholder="username" className="border p-3 rounded-lg" id="username" />
+        <input type="text" placeholder="password" className="border p-3 rounded-lg" id="password" />
+        <input type="text" placeholder="email" className="border p-3 rounded-lg" id="email" />
+        <button className="bg-slate-600 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Sign Up</button>
+      </form>
+      <div className='flex gap-2 items-center justify-center'>
+        <p>Have an account?</p>
+        <Link to={'/login'}><span className='text-blue-700'>Sign In</span></Link>
+      </div>
+    </div>
   )
 }
 
